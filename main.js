8e624d7fe3f1227d87af024adaa74a7c98654283
Daivik-tech <84041224+Daivik-tech@users.noreply.github.com>
2021-08-11 18:19:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var recognition= new SpeechRecognition;
+
+function start()
+{
+    document.getElementById("textbox").innerHTML="";
+    recognition.start()
+}
+recognition.onresult=function run(event)
+{
+console.log(event);
+var Content=even.results[0][0].transcript;
+console.log(Content);
+document.getElementById("textbox").innerHTML=Content;
+}
